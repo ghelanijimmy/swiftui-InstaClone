8b89e617d1458e923a19d57bd720e1fb1c163a3f
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2023-10-30 07:20:11 -0400

    Initial Commit

---
+++ InstaClone/InstaCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaCloneApp.swift
+//  InstaClone
+//
+//  Created by Jimmy Ghelani on 2023-10-30.
+//
+
+import SwiftUI
+
+@main
+struct InstaCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
